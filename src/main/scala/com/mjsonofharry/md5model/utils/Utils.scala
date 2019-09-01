16a93e91b39242aace4635890cb233f58a4ebd09
mjharrison <mjsonofharry@gmail.com>
2019-09-01 13:13:44 -0400

    formatting

--- src/main/scala/com/mjsonofharry/md5model/utils/Utils.scala
+++ src/main/scala/com/mjsonofharry/md5model/utils/Utils.scala
@@ -7,10 +7,12 @@ import java.text.DecimalFormat
 object Utils {
   val QUOTE = '"'
   def quotate(inside: String): String = QUOTE + inside + QUOTE
-  def keyValue[T](key: String, valueParser: Parser[T]) = string(key) ~ spaceChar ~> valueParser <~ whitespaces
+  def keyValue[T](key: String, valueParser: Parser[T]) =
+    string(key) ~ spaceChar ~> valueParser <~ whitespaces
   val quoteChar = char('"')
   val inQuotes = quoteChar ~> manyUntil(anyChar, quoteChar) map (_.mkString)
-  def vector[T](n: Int, valueParser: Parser[T]) = manyN(n, valueParser <~ {spaceChar || whitespaces})
+  def vector[T](n: Int, valueParser: Parser[T]) =
+    manyN(n, valueParser <~ { spaceChar || whitespaces })
   val whitespaces = many(whitespace)
   private val decimalFormatter = new DecimalFormat("#.##########")
   def format(x: Double) = decimalFormatter.format(x)
